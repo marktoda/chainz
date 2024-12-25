987ffe7756c088a8c3cc728b886102b402d597c2
Mark Toda <toda.mark@gmail.com>
2024-12-25 07:37:18 -0500

    feat: nixify

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Chainz";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f {
+          inherit system;
+          pkgs = nixpkgs.legacyPackages.${system};
+        });
+  in {
+    packages = forAllSystems ({pkgs, ...}: {
+      default = pkgs.callPackage ./default.nix {};
+    });
+    devShells = forAllSystems ({pkgs, ...}: {
+      default = pkgs.callPackage ./shell.nix {};
+    });
+  };
+}
