6d07c52a37e404bea36f694860cdf8690312fab5
Mark Toda <toda.mark@gmail.com>
2025-01-01 16:56:40 -0500

    fix: nix build

--- default.nix
+++ default.nix
@@ -5,5 +5,14 @@ in
     pname = manifest.name;
     version = manifest.version;
     cargoLock.lockFile = ./Cargo.lock;
+    # Some crates look for pkg-config to locate openssl.
+    nativeBuildInputs = [
+      pkgs.pkg-config
+    ];
+
+    # Provide OpenSSL to the build environment.
+    buildInputs = [
+      pkgs.openssl
+    ];
     src = pkgs.lib.cleanSource ./.;
   }
